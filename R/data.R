6b105d3c4b1cae15c668a48c76c645c1347c600f
Frederik Tiedemann <fj.tiedemann@googlemail.com>
2019-09-23 13:51:39 +0200

    first commit

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' 16th electoral term
+#' 
+#' A dataset containing role call votes cast during the 16th electoral term of the German Bundestag (18-10-2005 to 27-10-2009).
+#' 
+#' @format A data frame with 852 rows and 107 variables.
+#' \describe{
+#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Name}{Last name of voter}
+#' \item{Bezeichnung}{Full name of voter}
+#' ...
+#' }
+"bundestag16"
+
+#' 17th electoral term
+#' 
+#' A dataset containing role call votes cast during the 17th electoral term of the German Bundestag (27-10-2009 to 22-10-2013).
+#' 
+#' @format A data frame with 852 rows and 107 variables.
+#' \describe{
+#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Name}{Last name of voter}
+#' \item{Bezeichnung}{Full name of voter}
+#' ...
+#' }
+"bundestag17"
+
+#' 18th electoral term
+#' 
+#' A dataset containing role call votes cast during the 18th electoral term of the German Bundestag (22-10-2013 to 24-10-2017).
+#' 
+#' @format A data frame with 852 rows and 107 variables.
+#' \describe{
+#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Name}{Last name of voter}
+#' \item{Bezeichnung}{Full name of voter}
+#' ...
+#' }
+"bundestag18"
+
+#' 19th electoral term
+#' 
+#' A dataset containing role call votes cast during the 19th electoral term of the German Bundestag (24-10-2017).
+#' 
+#' @format A data frame with 852 rows and 107 variables.
+#' \describe{
+#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Name}{Last name of voter}
+#' \item{Bezeichnung}{Full name of voter}
+#' ...
+#' }
+"bundestag19"
