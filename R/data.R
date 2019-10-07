1d82b018021d1c305b61b6ba57432bd5ba4a63c5
Frederik Tiedemann <fj.tiedemann@googlemail.com>
2019-10-07 23:09:48 +0200

    wide -> long format

--- R/data.R
+++ R/data.R
@@ -1,59 +1,55 @@
 #' 16th electoral term
-#' 
+#'
 #' A dataset containing role call votes cast during the 16th electoral term of the German Bundestag (18-10-2005 to 27-10-2009).
-#' 
-#' All columns other than the ones described below are vote IDs and contain the following factors reflecting the voters' votes: `ja` - yes, `nein` - no, `ungültig` - invalid, `Enthaltung` - abstention, `nicht abgegeben` - vote not cast.
-#' 
-#' @format A data frame with 852 rows and 107 variables.
+#'
+#' @format A data frame with 88608 rows and 5 variables.
 #' \describe{
-#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Party}{Party associated with voter}
 #' \item{Name}{Last name of voter}
-#' \item{Bezeichnung}{Full name of voter}
-#' ...
+#' \item{Full Name}{Full name (plus constituency in case of duplicate names) of voter}
+#' \item{Vote ID}{ID of the role call vote, corresponding to electoral term and date}
+#' \item{Vote}{Vote - one of yes, no, invalid, abstained}
 #' }
 "bundestag16"
 
 #' 17th electoral term
-#' 
+#'
 #' A dataset containing role call votes cast during the 17th electoral term of the German Bundestag (27-10-2009 to 22-10-2013).
-#' 
-#' All columns other than the ones described below are vote IDs and contain the following factors reflecting the voters' votes: `ja` - yes, `nein` - no, `ungültig` - invalid, `Enthaltung` - abstention, `nicht abgegeben` - vote not cast.
-#' 
-#' @format A data frame with 852 rows and 107 variables.
+#'
+#' @format A data frame with 127766 rows and 5 variables.
 #' \describe{
-#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Party}{Party associated with voter}
 #' \item{Name}{Last name of voter}
-#' \item{Bezeichnung}{Full name of voter}
-#' ...
+#' \item{Full Name}{Full name (plus constituency in case of duplicate names) of voter}
+#' \item{Vote ID}{ID of the role call vote, corresponding to electoral term and date}
+#' \item{Vote}{Vote - one of yes, no, invalid, abstained}
 #' }
 "bundestag17"
 
 #' 18th electoral term
-#' 
+#'
 #' A dataset containing role call votes cast during the 18th electoral term of the German Bundestag (22-10-2013 to 24-10-2017).
-#' 
-#' All columns other than the ones described below are vote IDs and contain the following factors reflecting the voters' votes: `ja` - yes, `nein` - no, `ungültig` - invalid, `Enthaltung` - abstention, `nicht abgegeben` - vote not cast.
-#' 
-#' @format A data frame with 852 rows and 107 variables.
+#'
+#' @format A data frame with 141404 rows and 5 variables.
 #' \describe{
-#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Party}{Party associated with voter}
 #' \item{Name}{Last name of voter}
-#' \item{Bezeichnung}{Full name of voter}
-#' ...
+#' \item{Full Name}{Full name (plus constituency in case of duplicate names) of voter}
+#' \item{Vote ID}{ID of the role call vote, corresponding to electoral term and date}
+#' \item{Vote}{Vote - one of yes, no, invalid, abstained}
 #' }
 "bundestag18"
 
 #' 19th electoral term
-#' 
+#'
 #' A dataset containing role call votes cast during the 19th electoral term of the German Bundestag (24-10-2017).
-#' 
-#' All columns other than the ones described below are vote IDs and contain the following factors reflecting the voters' votes: `ja` - yes, `nein` - no, `ungültig` - invalid, `Enthaltung` - abstention, `nicht abgegeben` - vote not cast.
-#' 
-#' @format A data frame with 852 rows and 107 variables.
+#'
+#' @format A data frame with 66056 rows and 5 variables.
 #' \describe{
-#' \item{Fraktion/Gruppe}{Party associated with voter}
+#' \item{Party}{Party associated with voter}
 #' \item{Name}{Last name of voter}
-#' \item{Bezeichnung}{Full name of voter}
-#' ...
+#' \item{Full Name}{Full name (plus constituency in case of duplicate names) of voter}
+#' \item{Vote ID}{ID of the role call vote, corresponding to electoral term and date}
+#' \item{Vote}{Vote - one of yes, no, invalid, abstained}
 #' }
 "bundestag19"
